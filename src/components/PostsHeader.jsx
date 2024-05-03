9364d3e7d6485926d1cd37fdc109b92417ee4ae7
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-03 16:22:40 -0300

    fix: se corrigen estilos

--- src/components/PostsHeader.jsx
+++ src/components/PostsHeader.jsx
@@ -3,9 +3,9 @@ export const PostsHeader = () => {
         <header className="posts-header">
             <h3>Peliculas</h3>
             <section className="posts-header__links-container">
-                <a className="posts-header__link posts-header__link--selected link-regular" href="">Descubrir</a>
-                <a className="posts-header__link link-regular" href="">Populares</a>
-                <a className="posts-header__link link-regular" href="">Nuevas</a>
+                <a className="posts-header__link link link-regular link--selected" href="">Descubrir</a>
+                <a className="posts-header__link link link-regular" href="">Populares</a>
+                <a className="posts-header__link link link-regular" href="">Nuevas</a>
             </section>
         </header>
     )
