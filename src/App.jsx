fe1b7267252a9b52290f76b9aefcabaa060e4a5d
Christian Martinez <chmartinez.cba@gmail.com>
2024-05-03 16:52:29 -0300

    fix: import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* import React from 'react' */
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Navbar } from './components/navbar'
+import { Navbar } from './components/Navbar'
 import { LandingPage } from './pages/LandingPage'
 import { MovieDetailPage } from './pages/MovieDetailPage'
 
